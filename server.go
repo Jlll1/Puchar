5cd3205df7d82a6379aa939fb3be555bd9724adc
Jlll1 <arghantentua@tutanota.com>
2022-09-20 19:37:30 +0200

    Handle switching rounds in dashboard

--- server.go
+++ server.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"database/sql"
+	_ "embed"
 	"log"
 	"net/http"
-  _ "embed"
+	"strconv"
 
 	"github.com/Jlll1/Puchar/templates"
 	"github.com/labstack/echo"
@@ -35,7 +36,8 @@ func main() {
   }
 
   h := Handler{DB: db}
-  e.GET("/", h.getDashboard)
+  e.GET("/", h.getHome)
+  e.GET("/dashboard", h.getDashboard)
 
   e.Use(middleware.Logger())
   e.Logger.Fatal(e.Start(":5500"))
@@ -45,7 +47,16 @@ type Handler struct {
   DB *sql.DB
 }
 
+func (h *Handler) getHome(c echo.Context) error {
+  return c.HTML(http.StatusOK, templates.Home())
+}
+
 func (h *Handler) getDashboard(c echo.Context) error {
+  roundParam, err := strconv.Atoi(c.QueryParam("round"))
+  if err != nil {
+    c.Error(err)
+  }
+
   rows, err := h.DB.Query(`
 SELECT  "tournament"."title"
        ,"tournament"."subtitle"
@@ -63,13 +74,17 @@ SELECT  "tournament"."title"
     ON  "player1"."player_id" = "pairing"."first_player_id"
   JOIN  "player" AS "player2"
     ON  "player2"."player_id" = "pairing"."second_player_id"
- WHERE  "tournament"."tournament_id" = 1`)
+ WHERE  "tournament"."tournament_id" = 1
+   AND  "pairing"."round_id" = $1`, roundParam)
   if err != nil {
     return err
   }
   defer rows.Close()
 
-  model := templates.DashboardPage{Pairings: []templates.PairingModel{}}
+  model := templates.DashboardPage{
+    Pairings: []templates.PairingModel{},
+    SelectedRound: roundParam,
+  }
   for rows.Next() {
     p := templates.PairingModel{}
     err = rows.Scan(
